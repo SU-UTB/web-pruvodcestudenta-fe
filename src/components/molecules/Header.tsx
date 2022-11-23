2ebbe3a06c712482b8835f6d5e5d5389665108e1
David Sedlář <sedlardavid.cz@gmail.com>
2022-11-23 16:24:54 +0100

    tailwind added

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = () => {
-    return <div>
+    return <div className='h-24 w-full bg-orange-300'>
         Header
     </div>;
   };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "../components/molecules/Footer";
+import Header from "../components/molecules/Header";
 
 const Main = () => {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default Main;
