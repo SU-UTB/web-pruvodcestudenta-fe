d6ba69c1ea6e34be1b7e4d22ae4c06293c05414c
SedlarDavid <sedlardavid.cz@gmail.com>
2023-03-18 10:39:54 +0100

    rector section cleanup

---
+++ src/components/main/rector/RectorSectionWrapper.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const RectorSectionWrapper = styled.section`
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  column-gap: 82px;
+  padding-top: 48px;
+  padding-bottom: 48px;
+`
---
+++ src/components/main/rector/image/RectorImage.tsx
@@ -0,0 +1,8 @@
+import rector from "../../../../assets/images/rector.jpg";
+import React from "react";
+import {RectorImageWrapper} from "./RectorImageWrapper";
+
+
+export const RectorImage = () => {
+    return (<RectorImageWrapper src={rector}/>)
+}
