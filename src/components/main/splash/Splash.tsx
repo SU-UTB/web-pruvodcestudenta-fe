5a46f082c336cd5dd69624afe9d78a7515097594
SedlarDavid <sedlardavid.cz@gmail.com>
2023-03-18 10:51:12 +0100

    splash cleanup

--- src/components/main/splash/Splash.tsx
+++ src/components/main/splash/Splash.tsx
@@ -1,30 +1,13 @@
 import React from "react";
-import styled from "styled-components";
 import {CityPicker} from "../../CityPicker";
 import {Divider} from "../../Divider";
-
-
-const SplashWrapper = styled.section`
-
-  height: 540px;
-  width: 100vw;
-  background-color: #FF9F63;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: white;
-`
-
-const H1 = styled.h1`
-  font-style: normal;
-  font-weight: 900;
-  font-size: 62px;`
-
+import {SplashWrapper} from "./SplashWrapper";
+import {SplashH1} from "./SplashH1";
 
 
 export const Splash = () => <SplashWrapper>
     <Divider height={127}/>
-    <H1>Průvodce studenta splash screen</H1>
+    <SplashH1>Průvodce studenta splash screen</SplashH1>
     <Divider height={17}/>
     <p>Průvodce studenta UTB ti rychle pomůže zorientovat se na nové vysoké škole.</p>
     <Divider height={67}/>
---
+++ src/components/main/splash/SplashH1.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const SplashH1 = styled.h1`
+  font-style: normal;
+  font-weight: 900;
+  font-size: 62px;`
