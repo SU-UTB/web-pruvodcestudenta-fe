c8e62120386219d980d446f7dd0c313a25240627
Filip Tomeš <fitome@ttu.ee>
2023-04-02 21:36:57 +0300

    responsive splash screen section

--- src/components/main/splash/SplashH1.styled.tsx
+++ src/components/main/splash/SplashH1.styled.tsx
@@ -3,5 +3,12 @@ import styled from 'styled-components';
 export const SplashH1 = styled.h1`
   font-style: normal;
   font-weight: 900;
-  font-size: 62px;
+  font-size: 24px;
+  margin-top: 62px;
+  text-align: center;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: 62px;
+    margin-top: 120px;
+  }
 `;
