517d0a0e814a6c7dd6890f902e8f44eb4d26ba0c
Filip Tomeš <fitome@ttu.ee>
2023-04-03 18:34:29 +0300

    added hard spaces to avoid single characters being on the end of the line

--- src/components/main/rector/text/RectorText.tsx
+++ src/components/main/rector/text/RectorText.tsx
@@ -6,30 +6,31 @@ export const RectorText = () => {
       <p>Vážení studenti,</p>
       <br />
       <p>
-        rád bych vám osobně poděkoval, že jste se rozhodli zapsat a nastoupit ke
-        studiu na Univerzitě Tomáše Bati ve Zlíně navzdory nejistotě, která v
-        této době panuje.
+        rád bych vám osobně poděkoval, že jste se rozhodli zapsat
+        a&nbsp;nastoupit ke studiu na Univerzitě Tomáše Bati ve&nbsp;Zlíně
+        navzdory nejistotě, která v&nbsp;této době panuje.
       </p>
       <br />
       <p>
         Stojíte nyní na prahu nové životní etapy, abyste se naučili novým
-        znalostem a dovednostem, poznali nové kamarády, a také získali i
-        zahraniční zkušenost.
+        znalostem a&nbsp;dovednostem, poznali nové kamarády, a&nbsp;také získali
+        i&nbsp;zahraniční zkušenost.
       </p>
       <br />
       <p>
-        Tento průvodce vám pomůže se rychleji zorientovat v univerzitním
-        prostředí, a rovněž zpříjemnit váš studentský život.
+        Tento průvodce vám pomůže se rychleji zorientovat v&nbsp;univerzitním
+        prostředí, a&nbsp;rovněž zpříjemnit váš studentský život.
       </p>
       <br />
       <p>
-        Nebojte se v případě potřeby obrátit o pomoc na studijní oddělení či na
-        některou poradnu, nebo jen požádejte o radu staršího spolužáka. Rozhodně
-        se nenechte odradit od studia po prvních nezdarech u zkoušek, ale o to
-        více pilně studujte. Věřím, že za několik let sklidíte plody své práce.
+        Nebojte se v&nbsp;případě potřeby obrátit o&nbsp;pomoc na studijní
+        oddělení či na některou poradnu, nebo jen požádejte o&nbsp;radu staršího
+        spolužáka. Rozhodně se nenechte odradit od studia po prvních nezdarech
+        u&nbsp;zkoušek, ale o&nbsp;to více pilně studujte. Věřím, že za několik
+        let sklidíte plody své práce.
       </p>
       <br />
-      <p>Přeji vám pevné zdraví a hodně optimismu a sil!</p>
+      <p>Přeji vám pevné zdraví a&nbsp;hodně optimismu a&nbsp;sil!</p>
       <br />
       <br />
       <p>Milan Adámek, rektor</p>
