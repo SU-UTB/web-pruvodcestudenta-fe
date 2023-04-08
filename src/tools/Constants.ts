8110b57f10aeacc3e8bbeb82f83f576c8bf4ea54
SedlarDavid <sedlardavid.cz@gmail.com>
2023-04-08 15:34:11 +0200

    updated prod URL

--- src/tools/Constants.ts
+++ src/tools/Constants.ts
@@ -1,5 +1,6 @@
 export class Constants {
-    static API_BASE_URL: string = 'http://localhost/api';
+    static API_BASE_URL: string = 'https://rezervacesutb.wz.cz/api';
+    static API_TEST_URL: string = 'http://localhost/api';
     static SECTIONS: string = 'sections';
     static TOPICS: string = 'topics';
     static PAGES_LANDING: string = 'pages/landing';
