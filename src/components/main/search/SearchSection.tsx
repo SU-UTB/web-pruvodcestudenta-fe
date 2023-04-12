fac7411f933c1f7d1dd95b89b5d61ac0177fdae9
Filip Tomeš <fitome@ttu.ee>
2023-04-12 23:29:44 +0300

    improvment performance by using useMemo hook

--- src/components/main/search/SearchSection.tsx
+++ src/components/main/search/SearchSection.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import SearchButton from './SearchButton';
 import { SearchInput } from './SearchInput.styled';
 import { SearchInputForm } from './SearchInputForm.styled';
@@ -18,13 +18,14 @@ export const SearchSection = () => {
   const [tags] = useState<string[]>(pickedTags);
   const [query, setQuery] = useState<string>('');
 
-  const filteredTags = tags.filter((tag) => {
-    return tag.toLowerCase().includes(query.toLowerCase());
-  });
+  const filteredTags = useMemo(() => {
+    return tags.filter((tag) => {
+      return tag.toLowerCase().includes(query.toLowerCase());
+    });
+  }, [tags, query]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     // TODO: handle submit action if any
   };
 
