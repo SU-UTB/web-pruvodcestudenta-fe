81d15bc4ac342a79a957e8bfea1c4d155899eb93
Filip Tomeš <fitome@ttu.ee>
2023-05-19 18:53:06 +0300

    removed another unused part

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -1,13 +1,5 @@
-import styled from 'styled-components';
 import Footer from './footer/Footer';
 import Header from './header/Header';
-
-const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-  justify-content: center;
-`;
 
 interface Props {
   children: React.ReactNode;
