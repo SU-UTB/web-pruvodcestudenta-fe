9e530c5dee6ea9c55a46ff77db37427e7ef813bf
Filip Tomeš <fitome@ttu.ee>
2023-05-19 12:32:53 +0300

    added su and utb logos to the footer, updated styling

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,9 +1,20 @@
-import { FooterWrapper } from './Footer.styled';
+import { FooterSocialsWrapper, FooterWrapper } from './Footer.styled';
+
+import suLogo from '../../../assets/images/su-logo.svg';
+import utbLogo from '../../../assets/images/utb-logo.svg';
 
 const copyrightYear = new Date().getFullYear();
 
 const Footer = () => {
-  return <FooterWrapper>©STUDENTSKÁ UNIE UTB, {copyrightYear}</FooterWrapper>;
+  return (
+    <FooterWrapper>
+      <FooterSocialsWrapper>
+        <img src={suLogo} alt="Studentská Unie UTB" />
+        <img src={utbLogo} alt="Univerzita Tomáše Bati" />
+      </FooterSocialsWrapper>
+      <p>©STUDENTSKÁ UNIE UTB, {copyrightYear}</p>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
