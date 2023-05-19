9a6a629b6de0ddeb4cb605d29966c427f3b0b441
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2023-05-19 09:52:26 -0400

    Footer (#12)
    * added basic footer component
    
    * added su and utb logos to the footer, updated styling
    
    * changed naming to be more convenient
    
    * finished styling of footer logo link
    
    * removed component from usage in main page
    
    ---------
    
    Co-authored-by: Filip Tomeš <fitome@ttu.ee>

---
+++ src/components/layout/footer/Footer.styled.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.footer`
+  width: 100%;
+  padding: 20px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 4rem;
+`;
+
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+`;
