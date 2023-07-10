4ce90f69960bb1a701e90820a556d7c70e35d257
Filip Tomes <filiptomes@protonmail.com>
2023-07-10 16:18:46 +0200

    description styles

--- src/components/detail/description/Description.styled.tsx
+++ src/components/detail/description/Description.styled.tsx
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
 
 export const DescriptionWrapper = styled.section`
-  margin: 48px 134px;
+  width: 100%;
+  max-width: 1200px;
+  margin: 48px auto;
   display: flex;
   justify-content: center;
   align-items: center;
   border: 0.5px solid #e1e1e1;
-  /* shadow drop try */
-
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
-
   padding: 36px;
+  flex-wrap: wrap;
+  gap: 2rem;
 `;
 
 export const DescriptionContent = styled.article`
-  flex: 1;
   text-align: center;
+  width: 100%;
 `;
