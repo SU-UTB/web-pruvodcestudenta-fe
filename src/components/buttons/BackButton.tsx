0dc844d3fe62b07fb2de8d39bfa2a6e9923d4356
Filip Tomes <filiptomes@protonmail.com>
2023-08-02 16:24:55 +0200

    updated back button styling according to figma

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -8,6 +8,10 @@ export const BackButton = () => {
   return (
     <BackButtonWrapper title="Zpět" onClick={() => navigate(-1)}>
       Zpět
+      <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2 9L21 9" stroke="black" strokeWidth="2" strokeLinecap="round" />
+        <path d="M9 17L2 9L9 1" stroke="black" strokeWidth="2" strokeLinecap="round" />
+      </svg>
     </BackButtonWrapper>
   );
 };
