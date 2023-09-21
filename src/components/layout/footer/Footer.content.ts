3d7bdddd3569622f593cb592ca8daa071b9770bf
Filip Tomes <filiptomes@protonmail.com>
2023-09-21 22:25:10 +0200

    added link to the iOS app

--- src/components/layout/footer/Footer.content.ts
+++ src/components/layout/footer/Footer.content.ts
@@ -3,8 +3,13 @@ import { ILink } from '../../../lib/interfaces/ILink';
 export const footerLinks: Array<ILink> = [
   { title: 'UTB.cz', link: 'https://www.utb.cz', newTab: true },
   {
-    title: 'Aplikace UTB',
+    title: 'Aplikace UTB (Android)',
     link: 'https://play.google.com/store/apps/details?id=cz.unizone.utb&hl=cs&gl=US',
+    newTab: true,
+  },
+  {
+    title: 'Aplikace UTB (iOS)',
+    link: 'https://apps.apple.com/cz/app/moje-utb/id1598369807?l=cs',
     newTab: true,
   },
   {
