969bf1eef792e4a840a48ee3787204e1009ac0bb
Filip Tomes <filiptomes@protonmail.com>
2023-09-29 20:26:19 +0200

    added icons for footer links - aplikace UTB

--- src/components/layout/footer/Footer.content.ts
+++ src/components/layout/footer/Footer.content.ts
@@ -1,14 +1,18 @@
+import appleIcon from '../../../assets/icons/apple-logo.svg';
+import googlePlayIcon from '../../../assets/icons/google-playstore-logo.svg';
 import { ILink } from '../../../lib/interfaces/ILink';
 
 export const footerLinks: Array<ILink> = [
   { title: 'UTB.cz', link: 'https://www.utb.cz', newTab: true },
   {
-    title: 'Aplikace UTB (Android)',
+    title: 'Aplikace UTB',
+    icon: googlePlayIcon,
     link: 'https://play.google.com/store/apps/details?id=cz.unizone.utb&hl=cs&gl=US',
     newTab: true,
   },
   {
-    title: 'Aplikace UTB (iOS)',
+    title: 'Aplikace UTB',
+    icon: appleIcon,
     link: 'https://apps.apple.com/cz/app/moje-utb/id1598369807?l=cs',
     newTab: true,
   },
