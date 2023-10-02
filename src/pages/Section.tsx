432cce25675acb10f444180212e5c4d5946f4b0f
Filip Tomes <filiptomes@protonmail.com>
2023-10-02 15:28:16 +0200

    moved description component to shared components, section and topic now using the same description component

--- src/pages/Section.tsx
+++ src/pages/Section.tsx
@@ -2,17 +2,19 @@ import { useParams } from 'react-router-dom';
 
 import { BaseLayout } from '../components/layout/BaseLayout';
 import LoadingSpinner from '../components/loadingSpinner/LoadingSpinner';
-import { Description } from '../components/section/description/Description';
 import { SectionSplash } from '../components/section/splash/SectionSplash';
 import { BackButton } from '../components/shared/buttons/BackButton';
 import { CardSlider } from '../components/shared/cardSlider/CardSlider';
+import { Description } from '../components/shared/description/Description';
 import useFetchSection from '../hooks/section/useFetchSection';
 
 const Section = () => {
   const { sectionSlug = '' } = useParams();
   const { data: section, isLoading } = useFetchSection(sectionSlug);
 
-  const sortedTopics = section?.topics.sort((a,b) => (a.title > b.title) ? 1 : -1);
+  const sortedTopics = section?.topics.sort((a, b) =>
+    a.title > b.title ? 1 : -1,
+  );
 
   return (
     <BaseLayout>
